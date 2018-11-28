596603505b8025521db73765abc3a01c141c94bd
Logan Marshall <wildmovementco@gmail.com>
2018-11-27 17:50:05 -0700

    Connected to API to get app working

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './App.css';
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Post from './Post/Post';
+
+const baseUrl = "https://practiceapi.devmountain.com/api/posts";
 
 class App extends Component {
   constructor() {
@@ -19,19 +22,30 @@ class App extends Component {
   }
   
   componentDidMount() {
-
+    axios.get(baseUrl).then(response => {
+        this.setState({ posts: response.data })
+      })
   }
 
-  updatePost() {
-  
+  updatePost( id, text ) {
+    axios.put(`${baseUrl}?id=${id}`, {text}) //Why is text in {curly braces}?
+      .then(response => {
+        this.setState({ posts: response.data })
+      })
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    axios.delete(`${baseUrl}?id=${id}`)
+      .then(response => {
+        this.setState({ posts: response.data })
+      })
   }
 
-  createPost() {
-
+  createPost(text) {
+    axios.post(baseUrl, {text})
+      .then(response => {
+        this.setState({ posts: response.data })
+      })
   }
 
   render() {
@@ -43,7 +57,23 @@ class App extends Component {
 
         <section className="App__content">
 
-          <Compose />
+          <Compose 
+            createPostFn={ this.createPost }
+          />
+
+          {
+            posts.map(post => (
+              <Post 
+                key={ post.id }
+                text={ post.text } 
+                date={ post.date }
+                id={ post.id }
+                updatePostFn={ this.updatePost }
+                deletePostFn= { this.deletePost } 
+              />
+            ))
+          }
+
           
         </section>
       </div>
